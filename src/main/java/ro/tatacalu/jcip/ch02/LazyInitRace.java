a5f58935640dc176697e94237e4e20003967271f
Matei Dragu <tatacalu@users.noreply.github.com>
2013-10-17 21:24:02 +0200

    Chapter #2 - Section 2.2.2 - Race condition in lazy initialization

---
+++ src/main/java/ro/tatacalu/jcip/ch02/LazyInitRace.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ro.tatacalu.jcip.ch02;
+
+import ro.tatacalu.jcip.ch02.annotations.NotThreadSafe;
+
+/**
+ * @author tatacalu
+ * 
+ */
+@NotThreadSafe
+public class LazyInitRace {
+    private ExpensiveObject instance = null;
+
+    public ExpensiveObject getInstance() {
+        if (instance == null)
+            instance = new ExpensiveObject();
+        return instance;
+    }
+
+    // -------------------------------------------------------------------------
+    public class ExpensiveObject {
+    }
+}
